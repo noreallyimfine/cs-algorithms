4158f1f7814c11f9ede29f65194c4c45af0ef050
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-17 13:21:57 -0400

    passes sample but not yet tested

---
+++ moving_zeroes/moving_zeroes.py
@@ -0,0 +1,22 @@
+'''
+Write a function that takes an array of integers and moves each non-zero integer to the left side of the array, then returns the altered array. The order of the non-zero integers does not matter in the mutated array.
+'''
+
+def move_zeroes(arr):
+
+    # array may or may not have zeroes
+    # final order doesnt matter
+
+    # swap zeroes with last index thats not already a zero
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            j = len(arr) - 1
+            while j > i and arr[j] == 0:
+                j -= 1
+            temp = arr[j]
+            arr[j] = arr[i]
+            arr[i] = temp
+    return arr
+
+arr = [0, 3, 1, 0, -2]
+print(move_zeroes(arr))
