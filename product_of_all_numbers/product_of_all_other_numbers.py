9e54e5880ca8ac8058f77537d528843005957eca
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-17 13:32:50 -0400

    3rd problem pretty much solved, idk not tested

---
+++ product_of_all_numbers/product_of_all_other_numbers.py
@@ -0,0 +1,23 @@
+'''
+Write a function that receives an array of integers and returns an array consisting of the product of all numbers in the array except the number at that index.
+'''
+
+def product_of_all_other_numbers(arr):
+
+    products = []
+
+    # loop using indexer
+    for i in range(len(arr)):
+
+        # init sum
+        product = 0
+        # inner loop 
+        for j in range(len(arr)):
+            # if indexers not equal, multcrement sum
+            if j != i:
+                product *= arr[j]
+        # set val in new array
+        products.append(product)
+
+    return products
+
