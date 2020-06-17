4d214db18a28f0267ef87641e96b1f04e3e67b92
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-17 15:48:40 -0400

    begin sliding windows problem

---
+++ sliding_window_max/sliding_window_max.py
@@ -0,0 +1,7 @@
+'''
+Given an array of integers, there is a sliding window of size k which is moving from the left side of the array to the right, one element at a time. You can only interact with the k numbers in the window. Return an array consisting of the maximum value of each window of elements.
+'''
+
+def sliding_window_max(arr):
+
+    # indexer from second to second last
